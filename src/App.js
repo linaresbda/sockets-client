80555ec0ef9652432429feb2515b4c27ceb18524
Diego Linares <linaresbda@gmail.com>
2019-11-29 11:48:14 -0600

    feature: se agrega funcionalidad enviar y recibir mensajes (broadcast)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
 import './App.css';
+import io from 'socket.io-client';
 
 function App() {
+  let socket;
+
+  function handleNewConnection() {
+    if (!socket || socket.connected === false) {
+      console.log('new connection');
+      socket = io('http://192.168.15.13:80', { query: `user=${document.getElementById('inp-nickname').value}` });
+      listenerSocket();
+    }
+  }
+
+  function handleSendMessage() {
+    if (socket && socket.connected === true) {
+      let message = document.getElementById('inp-message').value;
+      let recipient = document.getElementById('inp-recipient').value;
+      if (message !== '') {
+        socket.emit('request', { message, recipient });
+        console.log('send message');
+      }
+    } else {
+      console.log('socket is not available');
+    }
+  }
+
+  function listenerSocket() {
+    socket.on('response', data => {
+      let txtMessage = document.getElementById('txt-messages');
+      let message = `${txtMessage.value}\n`;
+      message = message += data;
+      txtMessage.value = message;
+    });
+  }
+
   return (
     <div className="form-landing">
       {/* Nickname */}
       <label htmlFor="inp-nickname">Nickname:</label>
       <input type="text" id="inp-nickname" name="nickname" placeholder="type nickname here..." />
       <div className="div-form-button">
-        <button className="button" >new connection</button>
+        <button className="button" onClick={handleNewConnection}>new connection</button>
       </div>
       <hr className="hr-visible" />
       {/* List Messages */}
       <label htmlFor="messages">Messages</label>
-      <textarea disabled={true} readOnly name="messages" />
+      <textarea id='txt-messages' disabled={true} readOnly name="messages" />
       <hr className="hr-visible" />
       {/* Message */}
       <label htmlFor="inp-message">Message</label>
       <input type="text" id="inp-message" name="message" placeholder="type messages here..." />
       <div className="div-form-button">
-        <button className="button">send message</button>
+        <button className="button" onClick={handleSendMessage}>send message</button>
       </div>
       <hr className="hr-visible" />
       {/* Message recipient*/}
